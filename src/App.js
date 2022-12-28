ac1aacdf9e9bf48cdc614f0ab0a50ae2926c0a6b
Aaron Blum <blumaa@gmail.com>
2022-12-28 20:51:13 +0100

    feat: add snake-game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+
+/* Snake Game instructions*/
+
+const CANVAS_SIZE = [800, 800];
+const SNAKE_START = [
+  [8, 7],
+  [8, 8],
+];
+const APPLE_START = [8, 3];
+const SCALE = 40;
+const SPEED = 500;
+const DIRECTIONS = {
+  38: [0, -1], // up
+  40: [0, 1], // down
+  37: [-1, 0], // left
+  39: [1, 0], // right
+};
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [speed, setspeed] = useState(null);
+  const canvasRef = useRef(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <header>snake game</header>
+      <div className="game-window">
+        <canvas
+          ref={canvasRef}
+          width={`${CANVAS_SIZE[0]}`}
+          height={`${CANVAS_SIZE[1]}`}
+        />
+      </div>
+      <div>
+        <div>Controls:</div> <div>Score: {score}</div>
+      </div>
     </div>
   );
 }
